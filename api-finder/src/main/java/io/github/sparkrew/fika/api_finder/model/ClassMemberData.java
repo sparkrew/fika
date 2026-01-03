941d3e2aff0b42ca989d18c02d3eac30fdc0f31f
yogyagamage <47789154+yogyagamage@users.noreply.github.com>
2026-01-03 13:15:37 -0500

    Remove init clinit from test name

--- api-finder/src/main/java/io/github/sparkrew/fika/api_finder/model/ClassMemberData.java
+++ api-finder/src/main/java/io/github/sparkrew/fika/api_finder/model/ClassMemberData.java
@@ -1,9 +1,14 @@
 package io.github.sparkrew.fika.api_finder.model;
 
 import java.util.List;
+import java.util.Set;
 
 /**
- * Data class to hold class members (constructors, setters, getters).
+ * Data class to hold class members (constructors, setters, getters) and their required imports.
  */
-public record ClassMemberData(List<String> constructors, List<String> setters, List<String> getters) {
+public record ClassMemberData(
+        List<String> constructors, 
+        List<String> setters, 
+        List<String> getters,
+        Set<String> imports) {
 }
